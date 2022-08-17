03b98af92075830ed9bf64530d6715637317d736
Gabriel Thomaz <gabrielfthomaz@hotmail.com>
2022-08-17 10:26:38 -0300

    Initial Commit

---
+++ testeSwiftUI/testeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeSwiftUIApp.swift
+//  testeSwiftUI
+//
+//  Created by Gabriel Fernandes Thomaz on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct testeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
